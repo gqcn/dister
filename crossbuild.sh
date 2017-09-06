d0d4bd8568ad9a9df10a3e1de3760b70dba5a1fb
John <john@johng.cn>
2017-09-06 17:48:11 +0800

    shell脚本更新

---
+++ crossbuild.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+######################################
+#      用于跨平台交叉编译gluster         #
+######################################
+
+#./bin/cbuild.linux_amd64src/gapp/gluster/gluster.go --name=gluster --version=lastest
+./bin/cbuild.linux_amd64 src/gluster/gluster.go --name=gluster --version=0.6 --arch=386,amd64 --os=linux,windows
