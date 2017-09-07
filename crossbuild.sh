977dd4046da4d83aabee36eaeb1b6f029c84bb12
John <john@johng.cn>
2017-09-07 11:51:13 +0800

    细节优化

--- crossbuild.sh
+++ crossbuild.sh
@@ -4,4 +4,4 @@
 ######################################
 
 #./bin/cbuild.linux_amd64src/gapp/gluster/gluster.go --name=gluster --version=lastest
-./bin/cbuild.linux_amd64 src/gluster/gluster.go --name=gluster --version=0.6 --arch=386,amd64 --os=linux,windows
+./bin/cbuild.linux_amd64 src/gluster/gluster.go --name=gluster --version=0.6_beta --arch=386,amd64 --os=linux,windows
