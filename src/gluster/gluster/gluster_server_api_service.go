6a65549a3f0399b58dcaa06a9f64a2c5b2afba59
John <john@johng.cn>
2017-09-20 11:20:44 +0800

    完善gluster代码

--- src/gluster/gluster/gluster_server_api_service.go
+++ src/gluster/gluster/gluster_server_api_service.go
@@ -1,30 +1,3 @@
-// 目前支持两种类型服务的健康检查：mysql, web
-// 支持对每个服务节点使用自定义的check配置健康检查，check的格式参考示例。
-// check配置是非必需的，默认情况下gluster会采用默认的配置进行健康检查。
-// 注意：
-// 服务在gluster中也是使用kv存储，使用配置中的name作为键名，因此，每个服务的名称不能重复。
-//
-// 1、MySQL数据库服务：
-// {
-//     "name" : "Site Database",
-//     "type" : "mysql",
-//     "list" : [
-//         {"host":"192.168.2.102", "port":"3306", "user":"root", "pass":"123456", "database":"test"},
-//         {"host":"192.168.2.124", "port":"3306", "user":"root", "pass":"123456", "database":"test"}
-//     ]
-// }
-// 2、WEB服务：
-// {
-//     "name" : "Home Site",
-//     "type" : "web",
-//     "list" : [
-//         {"url":"http://192.168.2.102", "check":"http://192.168.2.102/health"},
-//         {"url":"http://192.168.2.124", }
-//     ]
-// }
-// 返回格式统一：
-// {result:1, message:"", data:""}
-
 package gluster
 
 import (
